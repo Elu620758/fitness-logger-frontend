6fc41753386c8cfefd962f6546c99677a93eb705
Elu620758 <elumalai620758@gmail.com>
2023-12-15 01:31:02 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import './App.css';
+import ForgotPassword from './components/ForgotPassword';
+import ResetPassword from './components/ResetPassword';
+import Home from './components/Home';
+import Allexcercisepage from './components/Allexcercisepage';
+import Cardio from './Pages/Cardio';
+import Aerobic from './Pages/Aerobic';
+import Resistance from './Pages/Resistance';
+import History from './Pages/History';
+import SingleExercise from "./Pages/SingleExercise";
+import ErrorPage from './Pages/ErrorPage';
+import LoginSignupPage from './components/LoginSignupPage';
+
 
 function App() {
+  const [user, setUser] = useState(null);
+  // console.log(user);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+      <div className="app-container">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login/*" element={<LoginSignupPage setUser={setUser} />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/reset-password" element={<ResetPassword />} />
+          <Route path="/allexcercisepage" element={<Allexcercisepage />} />
+          <Route path="/exercise/aerobic" element={<Aerobic user={user} />} />
+          <Route path="/exercise/cardio" element={<Cardio user={user} />} />
+          <Route path="/exercise/resistance" element={<Resistance user={user} />} />
+          <Route path="/history/:userId" element={<History user={user} />} />
+          <Route path="/history/:type/:id" element={<SingleExercise user={user} />} />
+          <Route path="/error" element={<ErrorPage />} />
+          <Route path="*" element={<Navigate to="/error" />} />
+        </Routes>
+      </Router>
+      </div> 
   );
 }
 
